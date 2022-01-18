267920e84a4149e3139d9e35c7384c4689a0d62c
VeronikaTan <90012301+VeronikaTan@users.noreply.github.com>
2022-01-17 16:36:24 -0800

    found missing element

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,37 @@
+
 import { galleryItems } from './gallery-items.js';
-// console.log(createGallery(galleryItems))
-
-const galleryDiv = document.querySelector('.gallery')
-
+// Change code below this line
+const divGallery = document.querySelector(".gallery");
 
 
-const createGallery = () => {
-   let markUp = galleryItems.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${original}"
-      data-source="${preview}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
-    })
-       .join('');
-    
-    galleryDiv.insertAdjacentHTML('beforeend', markUp);
-}
+const imgItem = galleryItems.map(item => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href=${item.original}>
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+    </div>`
+}).join("");
 
+divGallery.insertAdjacentHTML("afterbegin", imgItem);
 
-function dontClick(evt) {
-    evt.preventDefault();
-    if (evt.target.nodeName !== "IMG") {
+let openImage;
+
+divGallery.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
         return;
-    } else {
-        openImage = basicLightbox.create(`<img src="${evt.target.dataset.source}">`)
-        openImage.show()
     }
-}
-galleryDiv.addEventListener('click', dontClick)
+  openImage = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+openImage.show()
+});
 
-createGallery();
-   
+
+
+
+
+console.log(galleryItems);
